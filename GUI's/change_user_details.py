972a27a21a1b79bd55a4326995ed22b240d3170d
minas <bolaris.minas@gmail.com>
2023-06-06 21:06:07 +0300

    allages sto change_user_details.py

--- GUI's/change_user_details.py
+++ GUI's/change_user_details.py
@@ -6,7 +6,10 @@ class MyWindow(QtWidgets.QWidget):
         super().__init__()
         
         self.name = "Giannis"
-        self.username = "John"  # Initial username
+        self.username = "John" 
+        self.lastname = "Papadopoulos"
+        self.email = "johnpap@gmail.com"
+        self.vehicleid = "AZK-8309"
         
         self.setupUi()
     
@@ -23,6 +26,37 @@ class MyWindow(QtWidgets.QWidget):
         self.line_name = QtWidgets.QLineEdit(self)
         self.line_name.setText(self.name)
         self.layout.addWidget(self.line_name)
+
+
+    # Lastname
+
+        self.label_lastname = QtWidgets.QLabel("Lastname:", self)
+        self.layout.addWidget(self.label_lastname)
+
+        self.line_lastname = QtWidgets.QLineEdit(self)
+        self.line_lastname.setText(self.lastname)
+        self.layout.addWidget(self.line_lastname)
+
+        self.button_change_lastname = QtWidgets.QPushButton("Change Lastname", self)
+        self.button_change_lastname.clicked.connect(self.change_lastname)
+        self.layout.addWidget(self.button_change_lastname)
+
+
+    # Email
+
+        self.label_email = QtWidgets.QLabel("Email:", self)
+        self.layout.addWidget(self.label_email)
+
+        self.line_email = QtWidgets.QLineEdit(self)
+        self.line_email.setText(self.email)
+        self.layout.addWidget(self.line_email)
+
+        self.button_change_email = QtWidgets.QPushButton("Change Email", self)
+        self.button_change_email.clicked.connect(self.change_email)
+        self.layout.addWidget(self.button_change_email)
+      
+
+
     # Username
         self.label_username = QtWidgets.QLabel("Username:", self)
         self.layout.addWidget(self.label_username)
@@ -37,7 +71,15 @@ class MyWindow(QtWidgets.QWidget):
         self.line_password = QtWidgets.QLineEdit(self)
         self.line_password.setEchoMode(QtWidgets.QLineEdit.Password)
         self.layout.addWidget(self.line_password)
-    
+
+    # VehicleID
+        self.label_vehicleid = QtWidgets.QLabel("VehicleID:", self)
+        self.layout.addWidget(self.label_vehicleid)
+        self.line_vehicleid = QtWidgets.QLineEdit(self)
+        self.layout.addWidget(self.line_vehicleid)
+      
+   
+    # Buttons
         self.button_change_name = QtWidgets.QPushButton("Change Name", self)
         self.button_change_name.clicked.connect(self.change_name)
         self.layout.addWidget(self.button_change_name)
@@ -50,6 +92,12 @@ class MyWindow(QtWidgets.QWidget):
         self.button_change_password.clicked.connect(self.change_password)
         self.layout.addWidget(self.button_change_password)
         
+        
+        self.button_change_vehicleid = QtWidgets.QPushButton("Change VehicleID", self)
+        self.button_change_vehicleid.clicked.connect(self.change_vehicleid)
+        self.layout.addWidget(self.button_change_vehicleid)
+
+
         self.updateButton = QtWidgets.QPushButton("Update", self)
         self.updateButton.clicked.connect(self.updateNewInfo)
         self.layout.addWidget(self.updateButton)
@@ -59,8 +107,19 @@ class MyWindow(QtWidgets.QWidget):
     
     def change_name(self):
         new_name = self.line_name.text()
-        self.username = new_name
+        self.name = new_name
         QtWidgets.QMessageBox.information(self, "Name Changed", "Name has been changed to: {}".format(new_name))
+
+      
+    def change_lastname(self):
+        new_lastname = self.line_lastname.text()
+        self.lastname = new_lastname
+        QtWidgets.QMessageBox.information(self, "LastName Changed", "LastName has been changed to: {}".format(new_lastname))
+
+    def change_email(self):
+        new_email = self.line_email.text()
+        self.email = new_email
+        QtWidgets.QMessageBox.information(self, "Email Changed", "Email has been changed to: {}".format(new_email))
 
     def change_username(self):
         new_username = self.line_username.text()
@@ -71,9 +130,18 @@ class MyWindow(QtWidgets.QWidget):
         new_password = self.line_password.text()
         self.password = new_password
         QtWidgets.QMessageBox.information(self, "Password Changed", "Password has been changed to: {}".format(new_password))
+
+
+    def change_vehicleid(self):
+        new_vehicleid= self.line_username.text()
+        self.vehicleid = new_vehicleid
+        QtWidgets.QMessageBox.information(self, "VehicleID Changed", "VehicleID has been changed to: {}".format(new_vehicleid))
     
     def showCurrentInfo(self):
         QtWidgets.QMessageBox.warning(self, "Show User Details", "User details: Username - {}, Password - {}".format(self.username, self.password))
+
+    def editPersonalInfo(self):
+        QtWidgets.QMessageBox.warning(self, "Show Edit Details", "User details: Username - {}, Password - {}".format(self.username, self.password))
     
     def updateNewInfo(self):
         QtWidgets.QMessageBox.information(self, "Update", "Your details are updated")
